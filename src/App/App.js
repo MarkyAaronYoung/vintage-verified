11d178f8d37280e358f7f8fc12e96863ddcefaf6
Mark Young <markyaaronyoung@gmail.com>
2020-09-13 18:34:03 -0500

    google auth is setup and functions

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
         <h2>Vintage Verified</h2>
-        <button className="btn btn-info">I am a button</button>
+        <MyNavbar authed={authed}/>
       </div>
     );
   }
