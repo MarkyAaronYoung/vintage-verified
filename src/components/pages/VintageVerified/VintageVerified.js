e2f0406d424969624425037948e975ef64b173e2
Mark Young <markyaaronyoung@gmail.com>
2020-09-13 18:51:43 -0500

    added vintageverified component that shows on auth

---
+++ src/components/pages/VintageVerified/VintageVerified.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class VintageVerified extends React.Component {
+  render() {
+    return (
+      <div className="VintageVerified">
+        <h1>Vintage Verified</h1>
+      </div>
+    );
+  }
+}
+
+export default VintageVerified;
