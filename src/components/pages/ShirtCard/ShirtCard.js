58584105248ad25e3309573fef4840e4dce4b6a3
Mark Young <markyaaronyoung@gmail.com>
2020-09-20 17:10:58 -0500

    jeans now will display vintage based on if wheremde is equal to usa/mexico

--- src/components/pages/ShirtCard/ShirtCard.js
+++ src/components/pages/ShirtCard/ShirtCard.js
@@ -20,7 +20,7 @@ class ShirtCard extends React.Component {
     const { shirt } = this.props;
     const editShirtLink = `/shirts/${shirt.id}/edit/`;
     return (
-      <div className="card shirt-card text-white bg-dark mb-3 rounded">
+      <div className="card shirt-card text-white bg-dark mb-3 rounded" id="shirtcard"> 
         <img src={ shirt.imageUrl } className="card-img-top rounded-circle" alt="shirtPort"></img>
         <div className="card-body">
           <h2 className="card-title">{ shirt.shirtName }</h2>
