ec335068d19657c84b2cffd43ec12fc37e0ca383
Mark Young <markyaaronyoung@gmail.com>
2020-09-26 00:12:13 -0500

    app can now verify vintage by decade, fixed button color discrepancies

--- src/components/pages/JeanCard/JeanCard.js
+++ src/components/pages/JeanCard/JeanCard.js
@@ -24,6 +24,18 @@ class PantCard extends React.Component {
       }
       return <div>Not Vintage</div>;
     };
+
+    const vintageDecade = () => {
+      if (jean.whereMade === 'USA/Mexico' && jean.whatBrand === 'Levis' && jean.whatColorTab === 'Red') {
+        return <div>1940's</div>;
+      } if (jean.whatColorTab === 'Silver') {
+        return <div>1980's to 1990's</div>;
+      } if (jean.whatColorTab === 'Black' || jean.whatColorTab === 'Orange') {
+        return <div>1990's</div>;
+      }
+      return <div></div>;
+    };
+
     const editJeanLink = `/jeans/${jean.id}/edit`;
     return (
       <div className="card jean-card text-white mb-3 rounded mx-auto" id="jeancard">
@@ -34,7 +46,7 @@ class PantCard extends React.Component {
           <p className="card-text">Brand: { jean.whatBrand }</p>
           <p className="card-text">Fabric Type: { jean.fabricType }</p>
           <p className="card-text">Color Tab: { jean.whatColorTab }</p> */}
-          <h3 className="card-text">{vintageVerify()} { jean.isVintage }</h3>
+          <h3 className="card-text">{vintageVerify()} {vintageDecade()} { jean.isVintage }</h3>
           <div className="card-footer">
           <Link to={editJeanLink} className="btn btn-secondary" id="editbutton"><i className="far fa-edit">Edit</i></Link>
           <button type="button" className="btn btn-secondary"
--- src/components/pages/ShirtCard/ShirtCard.js
+++ src/components/pages/ShirtCard/ShirtCard.js
@@ -18,12 +18,31 @@ class ShirtCard extends React.Component {
 
   render() {
     const { shirt } = this.props;
+
     const vintageVerify = () => {
       if (shirt.whereMade === 'China/Taiwan' || shirt.whatBrand === 'Madewell' || shirt.stitchType === 'Double Stitch') {
         return <div>Not Vintage</div>;
       }
       return <div>Vintage</div>;
     };
+
+    const vintagePhil = () => {
+      if (shirt.whereMade ==='Phillipines') {
+        return <div>1990's</div>;
+      }
+      return <div></div>;
+    }
+
+    const vintageDecade = () => {
+      if (shirt.whereMade === 'USA/Korea/Hong Kong' && shirt.fabricType === 'Polyester(any amount)') {
+        return <div>1970's</div>;
+      }
+      else if (shirt.whereMade === 'USA/Korea/Hong Kong' && shirt.fabricType === 'All Cotton') {
+        return <div>1940's to 1960's</div>;
+      }
+      return <div></div>;
+    };
+
     const editShirtLink = `/shirts/${shirt.id}/edit/`;
     return (
       <div className="card shirt-card text-white mb-3 rounded mx-auto" id="shirtcard">
@@ -35,9 +54,9 @@ class ShirtCard extends React.Component {
           <p className="card-text">Brand: { shirt.whatBrand }</p>
           <p className="card-text">Stitch Type: { shirt.stitchType }</p>
           <p className="card-text">Fabric Type: { shirt.fabricType }</p> */}
-          <h3 className="card-text">{ vintageVerify()} { shirt.isVintage }</h3>
+          <h3 className="card-text">{vintageVerify()} {vintageDecade()}  {vintagePhil()}{ shirt.isVintage }</h3>
           <div className="card-footer">
-          <Link to={editShirtLink} className="btn btn-secondary"><i className="fas fa-edit" id="editbutton">Edit</i></Link>
+          <Link to={editShirtLink} className="btn btn-secondary" id="editbutton"><i className="fas fa-edit">Edit</i></Link>
           <button type="button" className="btn btn-secondary"
           onClick={this.deleteShirtEvent}><i className="fa fa-trash" aria-hidden="true">Delete</i></button>
           </div>
